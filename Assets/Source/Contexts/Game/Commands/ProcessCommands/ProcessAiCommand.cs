a28b0b18304c4bd9d78d59108411335fa4bcd360
rickinator9 <rickvisser94@hotmail.com>
2017-12-12 00:49:58 +0100

    Implemented some basic AI.

--- Assets/Source/Contexts/Game/Commands/ProcessCommands/ProcessAiCommand.cs
+++ Assets/Source/Contexts/Game/Commands/ProcessCommands/ProcessAiCommand.cs
@@ -1,7 +1,13 @@
-﻿using Assets.Source.Contexts.Game.Commands.Army;
+﻿using System.Linq;
+using Assets.Source.Contexts.Game.Commands.Army;
+using Assets.Source.Contexts.Game.Commands.War;
+using Assets.Source.Contexts.Game.Model.Country;
+using Assets.Source.Contexts.Game.Model.Map;
 using Assets.Source.Contexts.Game.Model.Player;
+using Assets.Source.Core.IoC;
 using strange.extensions.command.impl;
 using strange.extensions.signal.impl;
+using UnityEngine;
 
 namespace Assets.Source.Contexts.Game.Commands.ProcessCommands
 {
@@ -15,7 +21,16 @@ namespace Assets.Source.Contexts.Game.Commands.ProcessCommands
         public IPlayers Players { get; set; }
 
         [Inject]
+        public ICountries Countries { get; set; }
+
+        [Inject]
         public CreateArmySignal CreateArmyDispatcher { get; set; }
+
+        [Inject]
+        public CreateMovementPathSignal CreateMovementPathDispatcher { get; set; }
+
+        [Inject]
+        public WarDeclarationSignal WarDeclarationDispatcher { get; set; }
 
         public override void Execute()
         {
@@ -28,10 +43,64 @@ namespace Assets.Source.Contexts.Game.Commands.ProcessCommands
         public void ProcessAiPlayer(IPlayer aiPlayer)
         {
             var country = aiPlayer.Country;
-            if (country.Wars.Length > 0 && country.Armies.Length == 0)
+            if (country.Wars.Length == 0) DeclareWarOnClosestCountry(country);
+            else
             {
-                CreateArmyDispatcher.Dispatch(country.Location, country);
+                if (country.Armies.Length == 0) CreateArmy(country);
+                if (country.Armies.Any(army => !army.IsMoving)) MoveArmies(country);
             }
+        }
+
+        private void DeclareWarOnClosestCountry(ICountry country)
+        {
+            ICountry closestCountry = null;
+
+            foreach (var otherCountry in Countries.All)
+            {
+                if(otherCountry == country) continue;
+
+                var distance = Vector3.Distance(country.Location.Center, otherCountry.Location.Center);
+                if (closestCountry == null ||
+                    distance < Vector3.Distance(country.Location.Center, closestCountry.Location.Center))
+                    closestCountry = otherCountry;
+            }
+
+            if (closestCountry == null) return;
+
+            WarDeclarationDispatcher.Dispatch(new WarDeclarationParams {Attacker = country, Defender = closestCountry});
+        }
+
+        private void CreateArmy(ICountry country)
+        {
+            CreateArmyDispatcher.Dispatch(country.Location, country);
+        }
+
+        private void MoveArmies(ICountry country)
+        {
+            foreach (var army in country.Armies)
+            {
+                var war = country.Wars[0];
+                var primaryAttacker = war.GetEnemiesOfCountry(country)[0];
+                CreateMovementPathDispatcher.Dispatch(
+                    army,
+                    new MovementPathParams() { Start = army.Location, Destination = GetClosestUnoccupiedTile(army.Location, primaryAttacker.Territories) }
+                );
+            }
+        }
+
+        private IHexTile GetClosestUnoccupiedTile(IHexTile from, ILandTile[] tiles)
+        {
+            IHexTile closest = null;
+            foreach (var hexTile in tiles)
+            {
+                if (hexTile.Controller != hexTile.Country) continue;
+
+                var distance = Vector3.Distance(from.Center, hexTile.Center);
+                if (closest == null || distance < Vector3.Distance(closest.Center, from.Center))
+                    closest = hexTile;
+            }
+
+            return closest != null ? closest : from;
         }
     }
 }
